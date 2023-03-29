8cedc04316f60f1c3f259846e071eb730f7333c1
Ahmed <a7medalqalawyi@bmail.com>
2023-03-29 03:18:41 +0200

    after editing

--- lib/wedgits/carosal_item.dart
+++ lib/wedgits/carosal_item.dart
@@ -7,74 +7,79 @@ class CarosalItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: 250,
-            margin: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                image: DecorationImage(
-                    image: NetworkImage(
-                      url,),
-                    fit: BoxFit.cover)),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-            margin: EdgeInsets.only(top: 25, left: 30),
-            decoration: BoxDecoration(
-                color: Colors.blue,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(15)),
-            child: Text(
-              ' Sports ',
-              style: TextStyle(fontSize: 18, color: Colors.white),
+    return  InkWell(
+      onTap: (){
+        print('u pressed on Carosal part ');
+      },
+      child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 250,
+              margin: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                      image: NetworkImage(
+                        url,),
+                      fit: BoxFit.cover)),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.all(15),
-            alignment: Alignment.center,
-            width: double.infinity,
-            height: 200,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Row(
-                  children: [
-                    Text(
-                      'CNN Indonisia',
-                      style: TextStyle(fontSize: 18, color: Colors.white),
-                    ),
-                    SizedBox(width: 2,),
-                    Icon(
-                      Icons.verified,
-                      color: Colors.blue,
-                      size: 15,
-                    ),
-                    MySmallPoint(color: Colors.white, width: 5, height: 5),
-                    Text(
-                      '6 Hour ago',
-                      style: TextStyle(fontSize: 15, color: Colors.white),
-                    ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text(
-                      'programing is easy ',
-                      maxLines: 2,
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ],
-                ),
-              ],
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+              margin: EdgeInsets.only(top: 25, left: 30),
+              decoration: BoxDecoration(
+                  color: Colors.blue,
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(15)),
+              child: Text(
+                ' Sports ',
+                style: TextStyle(fontSize: 18, color: Colors.white),
+              ),
             ),
-          ),
-        ],
-      );
+            Container(
+              padding: EdgeInsets.all(15),
+              alignment: Alignment.center,
+              width: double.infinity,
+              height: 200,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        'CNN Indonisia',
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                      SizedBox(width: 2,),
+                      Icon(
+                        Icons.verified,
+                        color: Colors.blue,
+                        size: 15,
+                      ),
+                      MySmallPoint(color: Colors.white, width: 5, height: 5),
+                      Text(
+                        '6 Hour ago',
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'programing is easy ',
+                        maxLines: 2,
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+    );
   }
 }
--- lib/wedgits/recommendation_item.dart
+++ lib/wedgits/recommendation_item.dart
@@ -21,79 +21,85 @@ class RecommendationItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 120,
-            width: 125,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                image: DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage(
-                      newImgUrl
-                    ))),
-          ),
-          SizedBox(
-            width: 8,
-          ),
-          Expanded(
-            child: Container(
-              height: 130,
-              width: 130,
-              alignment: Alignment.topLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    category,
-                    style: TextStyle(fontSize: 13, color: Colors.grey),
-                  ),
-                  Spacer(flex: 1),
-                  Text(
-                    mainTxt,
-                    maxLines: 2,
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black),
-                  ),
-                  Spacer(flex: 1),
-                  Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 15,
-                        backgroundColor: Colors.white,
-                        backgroundImage: NetworkImage(
-                          profileImg,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 1,
-                      ),
-                      Text(
-                        name,
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
-                      ),
-                      MySmallPoint(
-                        color: Colors.grey,
-                        height: 4,
-                        width: 4,
-                      ),
-                      Text(
-                       date as String,
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
-              ),
+    return InkWell(
+
+      onTap: (){
+        print('InkWell pressed' );
+        },
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 120,
+              width: 125,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: NetworkImage(
+                        newImgUrl
+                      ))),
             ),
-          )
-        ],
+            SizedBox(
+              width: 8,
+            ),
+            Expanded(
+              child: Container(
+                height: 130,
+                width: 130,
+                alignment: Alignment.topLeft,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      category,
+                      style: TextStyle(fontSize: 13, color: Colors.grey),
+                    ),
+                    Spacer(flex: 1),
+                    Text(
+                      mainTxt,
+                      maxLines: 2,
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black),
+                    ),
+                    Spacer(flex: 1),
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 15,
+                          backgroundColor: Colors.white,
+                          backgroundImage: NetworkImage(
+                            profileImg,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 1,
+                        ),
+                        Text(
+                          name,
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                        MySmallPoint(
+                          color: Colors.grey,
+                          height: 4,
+                          width: 4,
+                        ),
+                        Text(
+                         date as String,
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
