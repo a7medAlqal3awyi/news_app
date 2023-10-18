407bcd675bde6f47e7be8482ecc65d89f42d609e
Ahmed <a7medalqalawyi@bmail.com>
2023-10-18 16:04:25 +0300

    finish appbar of home screen

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:news_app/providers/theme_providers.dart';
+import 'package:news_app/services/utils.dart';
 import 'package:news_app/widgets/drower_widget.dart';
 import 'package:provider/provider.dart';
 
@@ -16,27 +18,26 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context);
+    Color color = Utils(context).getColor;
     return Scaffold(
       appBar: AppBar(
- title: Text("News app",
-   style: GoogleFonts.lobster(
-       textStyle: TextStyle(fontSize: 20.sp,letterSpacing: .6)
-   ),),
-        elevation: 0,
-        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      ),
+          centerTitle: true,
+          iconTheme: IconThemeData(color: color),
+          title: Text(
+            "News app",
+            style:
+                  TextStyle(fontSize: 20.sp, letterSpacing: .6, color: color),
+
+          ),
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(IconlyLight.search),
+            ),
+          ]),
       drawer: const DrawerWidget(),
-      body: Center(
-        child: SwitchListTile(
-          title: Text(themeProvider.getDarkTheme ? "Dark" : "light"),
-          value: themeProvider.getDarkTheme,
-          onChanged: (bool value) {
-            setState(() {
-              themeProvider.setDarkTheme = value;
-            });
-          },
-        ),
-      ),
     );
   }
 }
---
+++ lib/services/utils.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/theme_providers.dart';
+
+class Utils{
+  BuildContext context ;
+
+  Utils(this.context);
+
+  bool get  getTheme => Provider.of<ThemeProvider>(context).getDarkTheme;
+  Color get getColor => getTheme ? Colors.white : Colors.black;
+}
